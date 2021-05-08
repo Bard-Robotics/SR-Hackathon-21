158363ddf626f08ea4884b34f90af6523eb5bd0f
Darrion <52856180+Darrionat@users.noreply.github.com>
2021-05-08 13:53:53 -0400

    CLI Script working!

--- generate_test.py
+++ generate_test.py
@@ -14,18 +14,23 @@ import subprocess
 import sys
 import os
 
-def curl(url, fname = None):
+import shutil
+
+
+def curl(url, fname=None):
     if fname is None:
         fname = url.split("/")[-1]
 
     if not os.path.exists(fname):
+        print(f"Downloading {fname}")
         with open(fname, 'wb') as outfile:
             data = requests.get(url)
             outfile.write(data.content)
+    else:
+        print(f"Using cached {fname}")
 
 
 FFT_SIZE = 1024
-NUM_FRAMES = 100
 BATCH_SIZE = 16
 
 if __name__ == "__main__":
@@ -40,34 +45,38 @@ if __name__ == "__main__":
     with open('ffhq.pkl', 'rb') as f:
         generator = pickle.load(f)['G_ema'].cuda()
 
-    print(generator.z_dim)
-
-    #z = torch.randn([1, generator.z_dim]).cuda()
-    #img = generator(z, None)
-
+    fname = sys.argv[-1]
     # Load soundfile in librosa
     (wave, sr) = librosa.load(sys.argv[-1], 11025, mono=True)
-    wave = wave[:FFT_SIZE*NUM_FRAMES]
-    
+    print("Loaded image and downsampled to 11025 hz")
+    num_samples = len(wave)
+    num_samples_round = FFT_SIZE * (num_samples // FFT_SIZE)
+    wave = wave[:num_samples_round]
     soundfile.write("output_trunc.wav", wave, sr)
 
-    # Calculate fourier transform 
-    spect = librosa.stft(wave, FFT_SIZE, FFT_SIZE//8)[:-1,:]
-    amp = np.abs(spect)/2.5
+    # Calculate fourier transform
+    spect = librosa.stft(wave, FFT_SIZE, FFT_SIZE // 4)
+    print(f"Spectrogram has shape {spect.shape}")
+    spect = spect[:512, :] / np.sqrt(FFT_SIZE)
+    amp = np.abs(spect) ** 2
+    amp = amp / np.max(amp)
     gan_in = amp.T
 
     num_images = gan_in.shape[0]
 
     try:
+        shutil.rmtree("images")
         os.mkdir("images")
     except FileExistsError:
         pass
 
+
     def run_batch(start, end):
-        tens = torch.from_numpy(gan_in[start:end,:]).cuda()
+        tens = torch.from_numpy(gan_in[start:end, :]).cuda()
         ims = generator(tens, None)
-        for i in range(end-start):
-            torchvision.utils.save_image(ims[i], f"images/sample{i+start:05d}.png")
+        for i in range(end - start):
+            torchvision.utils.save_image(ims[i], f"images/sample{i + start:05d}.png")
+
 
     batches = list(range(0, num_images, BATCH_SIZE)) + [num_images]
     batches = zip(batches, batches[1:])
@@ -75,10 +84,27 @@ if __name__ == "__main__":
         print(f"Processing image {s}/{num_images}")
         run_batch(s, e)
 
-    length = (FFT_SIZE*NUM_FRAMES) / sr
+    length = num_samples_round / sr
     fps = num_images / length
 
-    subprocess.run(["ffmpeg", "-framerate", str(fps), "-i", "images/sample%05d.png", "-i", "output_trunc.wav", "-c:v", "copy", "-map", "0:v:0", "-map", "1:a:0", "-c:a", "aac", "-b:a", "192k", "-vf", "format=yuv420p", "output.mp4"])
+    print(f"Have {length} seconds of audio.")
+    print(f"Have {num_images} frames")
+    print(f"Framerate: {fps}")
+    outname = ".".join(fname.split(".")[:-1]) + ".mp4"
+    ffmpeg_command = ["ffmpeg",
+                    "-framerate", str(fps),
+                    "-i", "images/sample%05d.png",
+                    "-i", "output_trunc.wav",
+                    "-c:v", "copy",
+                    "-map", "0:v:0",
+                    "-map", "1:a:0",
+                    "-c:a", "mp3",
+                    "-b:a", "192k",
+                    "-pix_fmt", "yuv420p",
+                    "-vcodec", "libx264",
+                    "-ar", "44100",
+                    outname]
 
-
-
+    os.remove("output_trunc.wav")
+    print(f"Calling {' '.join(ffmpeg_command)}")
+    subprocess.run(ffmpeg_command)
