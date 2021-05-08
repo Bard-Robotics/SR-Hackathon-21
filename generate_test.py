f6aad994c6cd7bd79cdc787a057744a18f71eb89
Rafie Walker <slickytail.mc@gmail.com>
2021-05-07 20:58:06 -0400

    Working generator

--- generate_test.py
+++ generate_test.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-
 import tensorflow as tf
 import numpy as np
 import tensorflow_hub as hub
+import librosa
+import sys
 
 #----- CelebA Progressive GAN 128 
 generate = hub.KerasLayer("https://tfhub.dev/google/progan-128/1")
-single = tf.random.normal([1, 510])
-tiled = tf.tile(single, [20, 1])
+# dim is 512
 
-path = tf.expand_dims(tf.range(-3, 3, 0.3), 1)
+if __name__ == '__main__':
+    (wave, sr) = librosa.load(sys.argv[-1], mono=True)
 
-added = tf.tile(path, [1, 2])
-dev = tf.concat([tiled, added], 1)
+    # Calculate fourier transform 
+    spect = librosa.stft(wave, 1024)[:-1,:100]
+    amp = np.abs(spect)**2
+    gan_in = amp.T
 
-images = generate(dev)
+    print(gan_in.shape)
+    images = generate(gan_in)
 
-for i in range(images.shape[0]):
-    tf.keras.preprocessing.image.save_img(f"sample{i}.png", images[i])
+    #for i in range(images.shape[0]):
+    #    tf.keras.preprocessing.image.save_img(f"images/sample{i}.png", images[i])
 
