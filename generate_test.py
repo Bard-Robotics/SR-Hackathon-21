54e511e7946ef5c3e6620a72d2784bac33493b04
Rafie Walker <slickytail.mc@gmail.com>
2021-05-08 12:33:02 -0400

    parameters

--- generate_test.py
+++ generate_test.py
@@ -24,6 +24,10 @@ def curl(url, fname = None):
             outfile.write(data.content)
 
 
+FFT_SIZE = 1024
+NUM_FRAMES = 100
+BATCH_SIZE = 16
+
 if __name__ == "__main__":
 
     # download pretrained netwok
@@ -43,36 +47,38 @@ if __name__ == "__main__":
 
     # Load soundfile in librosa
     (wave, sr) = librosa.load(sys.argv[-1], 11025, mono=True)
-    wave = wave[:1024*200]
+    wave = wave[:FFT_SIZE*NUM_FRAMES]
     
     soundfile.write("output_trunc.wav", wave, sr)
 
     # Calculate fourier transform 
-    spect = librosa.stft(wave, 1024, 128)[:-1,:]
+    spect = librosa.stft(wave, FFT_SIZE, FFT_SIZE//8)[:-1,:]
     amp = np.abs(spect)/2.5
     gan_in = amp.T
 
     num_images = gan_in.shape[0]
-    batch_size = 64
 
-    os.mkdir("images")
+    try:
+        os.mkdir("images")
+    except FileExistsError:
+        pass
 
     def run_batch(start, end):
         tens = torch.from_numpy(gan_in[start:end,:]).cuda()
-        ims = generator(tens)
+        ims = generator(tens, None)
         for i in range(end-start):
             torchvision.utils.save_image(ims[i], f"images/sample{i+start:05d}.png")
 
-    batches = list(range(0, num_images, batch_size)) + [num_images]
+    batches = list(range(0, num_images, BATCH_SIZE)) + [num_images]
     batches = zip(batches, batches[1:])
     for (s, e) in batches:
         print(f"Processing image {s}/{num_images}")
         run_batch(s, e)
 
-    length = (1024 * 100) / sr
+    length = (FFT_SIZE*NUM_FRAMES) / sr
     fps = num_images / length
 
-    subprocess.run(["ffmpeg", "-framerate", str(fps), "-i", "images/sample%05d.png", "-i", "output_trunc.wav", "-c:v", "copy", "-map", "0:v:0", "-map", "1:a:0", "-c:a", "aac", "-b:a", "192k", "output.mp4"])
+    subprocess.run(["ffmpeg", "-framerate", str(fps), "-i", "images/sample%05d.png", "-i", "output_trunc.wav", "-c:v", "copy", "-map", "0:v:0", "-map", "1:a:0", "-c:a", "aac", "-b:a", "192k", "-vf", "format=yuv420p", "output.mp4"])
 
 
 
